b541c92990ce0329b5c1506c2fc673ca053007d5
Or <60711866+oregev@users.noreply.github.com>
2023-12-14 18:48:57 +0200

    update 444

--- dist/src/index.js
+++ dist/src/index.js
@@ -5,12 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
+// import cors from 'cors';
 // import { appRouter } from './src/routes/config';
-const PORT = (_b = (_a = process === null || process === void 0 ? void 0 : process.env) === null || _a === void 0 ? void 0 : _a.PORT) !== null && _b !== void 0 ? _b : 8080;
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
+const PORT = (_b = (_a = process === null || process === void 0 ? void 0 : process.env) === null || _a === void 0 ? void 0 : _a.PORT) !== null && _b !== void 0 ? _b : 8080;
+// app.use(cors());
+// app.use(express.json());
 // app.use('/', appRouter);
 app.get('/', (req, res) => {
     return res.send('Express Typescript on Vercel');
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 // import { appRouter } from './src/routes/config';
 
-const PORT = process?.env?.PORT ?? 8080;
 const app = express();
+const PORT = process?.env?.PORT ?? 8080;
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
 // app.use('/', appRouter);
 
